3f90840a2613d60d4ec47afd4a7aca666c0cb5f4
Filip Broniek <broniekf@gmail.com>
2022-01-23 14:23:09 +0100

    zad9.1

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,8 @@
 import java.util.*;
 import Calendar.*;
-/**
- * This class is for tests
- *
- * @author  Filip Broniek
- */
 
 public class Main extends RuntimeException {
 
-    /**
-     * this is method for tests of features in Calendar
-     * @param args  array of arguments for working
-     */
     public static void main(String[] args) {
         MyDate myDate = new MyDate("1", "2", "2023");
         System.out.println(myDate.toString());
